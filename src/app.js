aaac16e3ee81b72feaa1dfb46e555803d5e750b9
Cristian <cristi.valentin29@gmail.com>
2021-03-19 19:11:28 +0100

    Allow Express to statically serve public files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const { json } = require("express");
 const express = require( "express" );
 const app = express();
 
+app.use( express.static( "public" ) );
+
 const PORT = process.env.PORT || 8080;
 
 app.get( "/", ( req, res ) => {
